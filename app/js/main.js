01b2372f1541a64cde3e43cd24e248f3e717cc00
Lucas Winkler <lucaswinkler@gmail.com>
2022-10-08 12:05:22 -0500

    Created an animations.scss and the projects now reveal on scroll

--- app/js/main.js
+++ app/js/main.js
@@ -3,9 +3,27 @@ const navToggle = document.querySelector('.nav__mobile-toggle');
 const navList = document.querySelector('.nav__list');
 
 navToggle.addEventListener('click', () => {
-  navList.hasAttribute('data-visible')
-    ? navToggle.setAttribute('aria-expanded', false)
-    : navToggle.setAttribute('aria-expanded', true);
-  navList.toggleAttribute('data-visible');
-  // header.toggleAttribute('data-overlay');
+	navList.hasAttribute('data-visible')
+		? navToggle.setAttribute('aria-expanded', false)
+		: navToggle.setAttribute('aria-expanded', true);
+	navList.toggleAttribute('data-visible');
+	// header.toggleAttribute('data-overlay');
 });
+
+function reveal() {
+	var reveals = document.querySelectorAll('.reveal');
+	for (var i = 0; i < reveals.length; i++) {
+		var windowHeight = window.innerHeight;
+		var elementTop = reveals[i].getBoundingClientRect().top;
+		var elementVisible = 50;
+
+		if (elementTop < windowHeight - elementVisible) {
+			reveals[i].classList.add('active');
+		} else {
+			reveals[i].classList.remove('active');
+		}
+	}
+}
+
+window.addEventListener('scroll', reveal);
+reveal();
