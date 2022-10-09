bd5cd8dd32f442d3cc5a98e41340ade4cb6376b6
Lucas Winkler <lucaswinkler@gmail.com>
2022-10-09 15:01:46 -0500

    Fixed small bugs with animations and fixed horizontal scrollbar when projects animate in

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,21 @@
 const header = document.querySelector('.header');
 const navToggle = document.querySelector('.nav__mobile-toggle');
 const navList = document.querySelector('.nav__list');
+const skillsItem = document.querySelectorAll('.skills__item');
+// const animationTimeRange = [1, 5];
 
-navToggle.addEventListener('click', () => {
-	navList.hasAttribute('data-visible')
-		? navToggle.setAttribute('aria-expanded', false)
-		: navToggle.setAttribute('aria-expanded', true);
-	navList.toggleAttribute('data-visible');
-	// header.toggleAttribute('data-overlay');
-});
+// function getRandomIntFromRange(range) {
+// 	return range[Math.floor(Math.random() * range.length)];
+// }
 
-// https://boosted.orange.com/v4-alpha/layout/responsive-utilities/
-// Use this to help rename responsive utility classes
+// function randomizeSkillsAnimationDelay() {
+// 	skillsItem.forEach(skill => {
+// 		skill.style.setProperty(
+// 			'--animation-order',
+// 			getRandomIntFromRange(animationTimeRange)
+// 		);
+// 	});
+// }
 
 // Reveals elements on scroll
 // Thanks to: https://alvarotrigo.com/blog/css-animations-scroll/
@@ -28,13 +32,23 @@ function reveal() {
 
 		// Separate the remove to it's own if statement to prevent
 		// reveal--active from constantly being added and removed on scroll
-    // due to elementTop changing on animation
-		if (!(elementTop < windowHeight - (elementVisible / 2))) {
+		// due to elementTop changing on animation
+		if (!(elementTop < windowHeight)) {
 			reveals[i].classList.remove('reveal--active');
 		}
 	}
 }
+
 window.addEventListener('scroll', reveal);
+
+navToggle.addEventListener('click', () => {
+	navList.hasAttribute('data-visible')
+		? navToggle.setAttribute('aria-expanded', false)
+		: navToggle.setAttribute('aria-expanded', true);
+	navList.toggleAttribute('data-visible');
+	// header.toggleAttribute('data-overlay');
+});
 
 // Check the position of elements on load
 reveal();
+// randomizeSkillsAnimationDelay();
