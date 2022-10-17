de5f75728fcf0898b3f823794e22b27c7c5444fa
Lucas Winkler <lucaswinkler@gmail.com>
2022-10-17 15:48:33 -0500

    Removed purose from manifest

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
         id: '/',
         short_name: 'Lucas Winkler',
         description:
-          'Lucas Winkler is a front-end developer who builds captivating web experiences.',
+          'Lucas Winkler is a front-end developer based in Canada, Ontario who builds captivating web experiences.',
         theme_color: '#181920',
         background_color: '#181920',
         theme_color: '#12022c',
@@ -26,13 +26,11 @@ export default defineConfig({
             src: '/images/icons/android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
-            purpose: 'maskable',
           },
           {
             src: '/images/icons/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'maskable',
           },
         ],
       },
