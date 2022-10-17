2d0924a2a4955fc76aa12bfd74e2859d1e80c58f
Lucas Winkler <lucaswinkler@gmail.com>
2022-10-17 10:50:00 -0500

    Added vite + netlify config to config the vite pwa plugin

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ['**/*.{png}'],
+      manifest: {
+        name: 'Lucas Winkler | Front-End Developer',
+        start_url: '/',
+        id: '/',
+        short_name: 'Lucas Winkler',
+        description:
+          'Lucas Winkler is a front-end developer who builds captivating web experiences.',
+        theme_color: '#181920',
+        icons: [
+          // {
+          //   src: '/images/icons/chrome192.png',
+          //   sizes: '192x192',
+          //   type: 'image/png',
+          // },
+          {
+            src: '/images/icons/favicon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
