3c3aff207d99e8950aac296a6bacd91534a1f510
Lucas Winkler <lucaswinkler@gmail.com>
2022-10-17 16:36:47 -0500

    Added maskable purpose to manifest

--- vite.config.js
+++ vite.config.js
@@ -29,11 +29,13 @@ export default defineConfig({
             src: '/images/icons/android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
+            purpose: 'maskable',
           },
           {
             src: '/images/icons/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
+            purpose: 'maskable',
           },
         ],
       },
