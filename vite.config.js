08b4ae0c3e5f268b13f16546cc3aac0d9b4fcdbf
Lucas Winkler <lucaswinkler@gmail.com>
2022-10-23 20:45:58 -0500

    Added google fonts to workbox runtime caching

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,36 @@ export default defineConfig({
         enabled: true,
       },
       workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'gstatic-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
         globPatterns: [
           '**/*.{js,css,html,webp,svg,png,jpg,jpeg,ico}',
           'sitemap.xml',
