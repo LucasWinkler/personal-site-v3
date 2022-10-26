4cfd34879c11d2dc98bd02f62630fa01bb57f795
Lucas Winkler <lucaswinkler@gmail.com>
2022-10-26 14:11:02 -0500

    Modified favicons again

--- vite.config.js
+++ vite.config.js
@@ -71,16 +71,13 @@ export default defineConfig({
             src: 'android-chrome-144x144.png',
             sizes: '144x144',
             type: 'image/png',
+            purpose: 'maskable',
           },
           {
             src: 'android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
-          },
-          {
-            src: 'android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
+            purpose: 'maskable',
           },
           {
             src: 'android-chrome-512x512.png',
