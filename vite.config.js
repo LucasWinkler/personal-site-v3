2efed5bafeb87c82baa6632696e6e644fd8d555a
Lucas Winkler <lucaswinkler@gmail.com>
2022-11-05 10:32:24 -0500

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -117,6 +117,12 @@ export default defineConfig({
             type: 'image/png',
             purpose: 'maskable',
           },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable any',
+          },
         ],
       },
     }),
